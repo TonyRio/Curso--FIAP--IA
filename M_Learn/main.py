8d3d40ec5341419efffec99ac478507eda564804
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-11 15:58:00 -0300

    inicio de estudos

---
+++ M_Learn/main.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import  load_diabetes
+from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pylab as plt
