0d5a7eaeec23cce6d77017e75f704b5774c8f4fc
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-12 00:11:07 -0300

    Update Miner_ibm.py

--- IBM/Miner_ibm.py
+++ IBM/Miner_ibm.py
@@ -1,2 +1,6 @@
 import json
 import watson
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 import
+Features, ConceptsOptions, RelationsOptions, EmotionOptions, EntitiesOptions, SemanticRolesOptions, SentimentOptions
