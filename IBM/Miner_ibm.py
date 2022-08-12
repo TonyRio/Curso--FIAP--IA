6c3e34af9ddea551f6f454ccd0e1741c783dd345
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-12 19:34:57 -0300

    ciencia de dados - saude

--- IBM/Miner_ibm.py
+++ IBM/Miner_ibm.py
@@ -1,3 +1,10 @@
-import json
-import flask
-#fim
+#importacoes
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("/content/Heart_data.csv")
+# TOP 5
+dataset.head()
