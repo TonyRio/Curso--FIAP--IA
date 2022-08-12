b423bc6679f425264bf40341a20b5241ea074622
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-12 00:24:59 -0300

    cont

--- IBM/Miner_ibm.py
+++ IBM/Miner_ibm.py
@@ -1,6 +1,2 @@
 import json
-import watson
-import json
-from watson_developer_cloud import NaturalLanguageUnderstandingV1
-from watson_developer_cloud.natural_language_understanding_v1 import
-Features, ConceptsOptions, RelationsOptions, EmotionOptions, EntitiesOptions, SemanticRolesOptions, SentimentOptions
+import flask
