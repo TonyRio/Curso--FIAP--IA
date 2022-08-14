db2a1fa2685bd71fc2dfb55fc22e5f41bb87f979
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-14 01:25:22 -0300

    variaveis de pesquisas

--- Ciencia_de_Dados/DCP -Edu/Hands_On/miner_1.py
+++ Ciencia_de_Dados/DCP -Edu/Hands_On/miner_1.py
@@ -1,4 +1,16 @@
 import pandas as pd
-df = pd.read_csv("bancos.csv")
-print(df.head)
-print(df.info)
+df = pd.read_csv("vgsales.csv")
+#print(df.head)
+#print(df.sample())
+#print(df.tail())
+#print(df.info)
+#print(df.describe())
+print(df.head())
+
+# CONSULTA COM FILTRO DE DADOS
+print(df.query('Global_Sales>30'))
+
+# CONSULTA ESPECIFICA
+#print(df.loc[5,'Global_Sales'])
+
+
