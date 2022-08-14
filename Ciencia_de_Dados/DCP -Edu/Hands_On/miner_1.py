cfd983c0f05e22db4571774dc1ec405d1b970d01
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-14 01:48:45 -0300

    Update miner_1.py

--- Ciencia_de_Dados/DCP -Edu/Hands_On/miner_1.py
+++ Ciencia_de_Dados/DCP -Edu/Hands_On/miner_1.py
@@ -5,12 +5,25 @@ df = pd.read_csv("vgsales.csv")
 #print(df.tail())
 #print(df.info)
 #print(df.describe())
-print(df.head())
+#print(df.head())
 
 # CONSULTA COM FILTRO DE DADOS
-print(df.query('Global_Sales>30'))
+#print(df.query('Global_Sales>30'))
 
 # CONSULTA ESPECIFICA
 #print(df.loc[5,'Global_Sales'])
+#print(df.loc [2],[4], [2],[3])
+
+# MOSTRA VALORES DISTINTOS DAS VARIAVEIS - quantidade de dados pela classe
+#print(df.nunique())
+#print(df['Genre'].unique())
+
+# MOSTRA VALORES Null
+#print(df.isnull)
+print(df.isnull().sum())
+
+# MOSTRA AS LINHAS QUE ESTÃO MISSING
+df2 = df[df['Year'].isnull()]
+print (df2)
 
 
