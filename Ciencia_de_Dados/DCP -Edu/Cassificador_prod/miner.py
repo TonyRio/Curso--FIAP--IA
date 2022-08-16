1a6b830e0f44e672c87a81281925d2b4d4fad03d
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-15 23:30:43 -0300

    1

--- Ciencia_de_Dados/DCP -Edu/Cassificador_prod/miner.py
+++ Ciencia_de_Dados/DCP -Edu/Cassificador_prod/miner.py
@@ -1,6 +1,14 @@
 import gzip
 import json
-
+import warnings
+import gensim
+from collections import Counter
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
 
 #Carregar Data Set
 
@@ -9,4 +17,53 @@ with gzip.open('ecommerce.json.gz') as fp:
     for line in fp:
         entry =line.decode('utf8')
         corpus.append(json.loads(entry))
-print(corpus[0]['name'])
+#print(corpus[0]['name'])
+print(len(corpus))
+#print(gensim.summarization.summarize(corpus[2]['descr']))
+
+#construindo um datase com somente 10.000 primeiros produtos
+dataset = list()
+for entry in corpus[:10000]:
+    if 'cat' in entry:
+        dataset.append(((entry['name'], entry['cat'.lower().strip()])))
+#print (dataset)
+#print(len(dataset))
+#print(dataset[:2])
+
+# QUANTAS CATEGORIAS DISTINTAS NOS TEMOS E QUANTOS ITENS POR CATEGORIA
+counter = Counter([cat for prod, cat in dataset])
+print(counter.most_common())
+
+stopwords = nltk.download('stopwords')
+
+stopwords = nltk.corpus.stopwords.words('portuguese')
+print(stopwords)
+
+#CONSTRUINDO MODELOS PREDITIVOS
+
+# MODELO SVM COM PIPELINE
+modelo = Pipeline([('vect', TfidfVectorizer()), ('clf', SVC(kernel = 'linear', probability = True))])
+LabelEncoder
+
+# OBJETO PARA NORMALIZACAO DE LABELS
+encoder = LabelEncoder()
+encoder
+
+# OBTENDO DADOS DAS LABELS
+data = [prod for prod, cat in dataset]
+labels = [cat for prod, cat in dataset]
+print (len(data))
+
+# NORMALIZACAO DO LABELS
+target = encoder.fit_transform(labels)
+print (target)
+print(encoder.classes_.item(2))
+
+# FIT DO MODELO
+modelo.fit(data, target)
+
+# PREVENDO CATEGORIA A PARTIR DA DESCRICAO
+print (modelo.predict(["caneta"]))
+print(encoder.classes_[3])
+
+
