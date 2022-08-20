9e1e8d3bf682fdb9a7c87abf2eba87e0a592e470
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-08-20 20:25:11 -0300

    Update Miner_NAive_Bayes.py

--- Ciencia_de_Dados/DCP -Edu/Classfc_Doc_Aut/Miner_NAive_Bayes.py
+++ Ciencia_de_Dados/DCP -Edu/Classfc_Doc_Aut/Miner_NAive_Bayes.py
@@ -7,4 +7,15 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
 import warnings
-warnings.filterwarnings("Ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+#DEFININDO SOMENTE 4 CATEGORIAS
+categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+
+#TREINAMENTO
+twenty_train = fetch_20newsgroups(subset='train', categories = categories, shuffle=True, random_state=42)
+#print(twenty_train)
+
+#CLASSES
+print(twenty_train.target_names)
+print(len(twenty_train.data))
